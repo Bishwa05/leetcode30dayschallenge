1c5f9cc95342112bf2a96cd43b9271a956ca89f4
Bishwa <bishwa.nayak@sap.com>
2020-04-22 13:52:28 +0530

    Day 22.

--- src/week4/SubarraySumEqualsK.java
+++ src/week4/SubarraySumEqualsK.java
@@ -1,6 +1,6 @@
 package week4;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class SubarraySumEqualsK {
 
@@ -23,9 +23,24 @@ public class SubarraySumEqualsK {
         return sumCount;
     }
 
+    //optimised
+    public static int subarraySum2(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap < Integer, Integer > map = new HashMap<>();
+        map.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
     public static void main(String arg[]){
         int [] nums = {-1,-1,1};
         int k = 0;
-        System.out.println(subarraySum(nums, k));
+        System.out.println(subarraySum2(nums, k));
     }
 }
