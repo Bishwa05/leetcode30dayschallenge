3cab0084aa0320fc76301b44653e441c230a4a6b
Bishwa <bishwa.nayak@sap.com>
2020-04-25 13:21:13 +0530

    Day 25.

--- src/week4/JumpGame.java
+++ src/week4/JumpGame.java
@@ -1,5 +1,26 @@
 package week4;
 
+/**
+ *
+ * Given an array of non-negative integers, you are initially positioned at the first index of the array.
+ *
+ * Each element in the array represents your maximum jump length at that position.
+ *
+ * Determine if you are able to reach the last index.
+ *
+ * Example 1:
+ *
+ * Input: [2,3,1,1,4]
+ * Output: true
+ * Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+ * Example 2:
+ *
+ * Input: [3,2,1,0,4]
+ * Output: false
+ * Explanation: You will always arrive at index 3 no matter what. Its maximum
+ *              jump length is 0, which makes it impossible to reach the last index.
+ *
+ */
 public class JumpGame {
 
     public boolean canJump(int[] nums) {
@@ -14,7 +35,8 @@ public class JumpGame {
     }
 
     public static void main(String arg[]){
-        int []arr = {2,3,1,1,4};
+        // int []arr = {2,3,1,1,4};
+        int []arr = {3,2,1,0,4};
         JumpGame j = new JumpGame();
         System.out.println(j.canJump(arr));
     }
