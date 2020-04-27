89d285ceaff97c70c15afc39d05f48e4fde8bc11
Bishwa <bishwa.nayak@sap.com>
2020-04-28 01:09:53 +0530

    experiments with DP;

--- src/week4/LCS.java
+++ src/week4/LCS.java
@@ -1,7 +1,52 @@
 package week4;
 
+import javafx.util.Pair;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class LCS {
-    public int longestCommonSubsequence(String text1, String text2) {
+
+    /**
+     * The recursive solution
+     * Time complexity : O(2^m *(m+n))
+     */
+    public int longestCommonSubsequence(String text1, String text2){
+        if(text1.isEmpty()|| text2.isEmpty()) return 0;
+
+        if(text1.charAt(0)== text2.charAt(0)){
+            return 1+ longestCommonSubsequence(text1.substring(1), text2.substring(1));
+        }
+
+        return Math.max(longestCommonSubsequence(text1,text2.substring(1)),
+                longestCommonSubsequence(text1.substring(1),text2));
+    }
+
+
+    Map<Pair<String,String>,Integer> cache = new HashMap<>();
+
+    public int longestCommonSubsequenceMemo(String text1, String text2){
+        if(text1.isEmpty()|| text2.isEmpty()) return 0;
+
+        Pair<String,String> x = new Pair(text1,text2);
+
+        if(cache.containsKey(x)) return cache.get(x);
+        if(text1.charAt(0)== text2.charAt(0)){
+            return 1+ longestCommonSubsequenceMemo(text1.substring(1), text2.substring(1));
+        }
+
+        cache.put(x,Math.max(longestCommonSubsequenceMemo(text1,text2.substring(1)),
+                longestCommonSubsequenceMemo(text1.substring(1),text2)));
+
+        return cache.values().stream().max((a,b) -> (a>b)?a:b).orElse(0);
+    }
+
+    /**
+     *
+     * DP bottom up approach
+     *
+     */
+    public int longestCommonSubsequenceDP(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();
         int dp[][] = new int [n+1][m+1];
@@ -29,6 +74,9 @@ public class LCS {
         LCS l = new LCS();
         System.out.println(l.longestCommonSubsequence("abbc",
                 "abc"));
+        System.out.println(l.longestCommonSubsequenceMemo("bl",
+                "yby"));
+
 
     }
 }
