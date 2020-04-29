75eba28e3e847b6c43a3255159984d2a7f4cf3f6
Bishwa <bishwa.nayak@sap.com>
2020-04-29 17:08:39 +0530

    day 29, accepted solution.

---
+++ src/week4/MaxPathSum.java
@@ -0,0 +1,35 @@
+package week4;
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+    }
+ }
+public class MaxPathSum {
+    int maxPathSum;
+    public int maxPathSum(TreeNode root) {
+        maxPathSum =Integer.MIN_VALUE;
+        pathSum(root);
+        return maxPathSum;
+    }
+
+    public int pathSum(TreeNode node) {
+        if(node == null) return 0;
+
+        int left = Math.max(0,pathSum(node.left));
+        int right = Math.max(0, pathSum(node.right));
+        maxPathSum = Math.max(maxPathSum, left+right+node.val);
+        return Math.max(left, right) + node.val;
+    }
+
+    public static void main(String arg[]){
+
+    }
+}
