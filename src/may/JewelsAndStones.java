19a4b64de7beb36401ae76c472cdf40b6bb90774
Bishwa <bishwa.nayak@sap.com>
2020-05-02 14:02:55 +0530

    day 2.

---
+++ src/may/JewelsAndStones.java
@@ -0,0 +1,24 @@
+package may;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JewelsAndStones {
+
+    public int numJewelsInStones(String J, String S) {
+
+        Set<Character> charSet = new HashSet<>();
+
+        for(char a : J.toCharArray()){
+            charSet.add(a);
+        }
+        int count =0;
+        for(int i =0; i<S.length(); i++){
+            if(charSet.contains(S.charAt(i))){
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
