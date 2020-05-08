9b710f4d9b6642034f82605c50a4e657dc112a9e
Bishwa <bishwa.nayak@sap.com>
2020-05-08 14:53:44 +0530

    day 8.

--- src/may/TreeNode.java
+++ src/may/TreeNode.java
@@ -1,9 +1,9 @@
 package may;
 
-class TreeNode{
-             int val;
-             TreeNode left;
-             TreeNode right;
-             TreeNode() {}
-             TreeNode(int val) { this.val = val; }
+public class TreeNode{
+             public int val;
+             public TreeNode left;
+             public TreeNode right;
+             public TreeNode() {}
+             public TreeNode(int val) { this.val = val; }
         }
