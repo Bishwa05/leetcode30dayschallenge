8c676ee3ba2bc69fe2e232833c280a6284578987
Bishwa <bishwa.nayak@sap.com>
2020-05-22 23:48:31 +0530

    day 22.

--- src/may/week4/SortCharactersByFrequency.java
+++ src/may/week4/SortCharactersByFrequency.java
@@ -46,6 +46,33 @@ public class SortCharactersByFrequency {
         return sb.toString();
     }
 
+    public String frequencySortOptimized(String s){
+        if(s.isEmpty() || s.length()==0)
+            return s;
+        int[] map = new int[128];
+        char[] cA = s.toCharArray();
+
+        for(char c:cA)
+            map[c]++;
+
+        char[] result = new char[s.length()];
+        for(int i=0;i<s.length();){
+            int max = 0;
+            int index = 0;
+            for(int j = 0;j<128;j++){
+                if(map[j]>max){
+                    max = map[j];
+                    index = j;
+                }
+            }
+            while(map[index]>0){
+                result[i++] = (char)index;
+                map[index]--;
+            }
+        }
+        return new String(result);
+
+    }
     public static void main(String arg[]){
 
         SortCharactersByFrequency s = new SortCharactersByFrequency();
