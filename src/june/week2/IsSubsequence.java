ea15627b9dca4498b66adb479db4ea5606214054
Bishwa <bishwa.nayak@sap.com>
2020-06-09 21:23:54 +0530

    day 9

---
+++ src/june/week2/IsSubsequence.java
@@ -0,0 +1,14 @@
+package june.week2;
+
+public class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int j=0;
+        for(int i=0; i<t.length() && j<s.length(); i++){
+            if(s.charAt(j)== t.charAt(i)){
+                j++;
+            }
+        }
+        return (j== s.length());
+
+    }
+}
