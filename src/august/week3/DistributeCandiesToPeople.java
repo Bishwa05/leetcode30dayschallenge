0b0b500fcac7fcb7978978ba35242e1329db7f48
Bishwa <bishwa.nayak@sap.com>
2020-08-17 21:52:38 +0530

    day4

---
+++ src/august/week3/DistributeCandiesToPeople.java
@@ -0,0 +1,29 @@
+package august.week3;
+
+public class DistributeCandiesToPeople
+{
+
+    public int[] distributeCandies(int candies, int num_people){
+            int[] res = new int[num_people];
+
+            int i =0;
+            int loops =0;
+
+            while(candies>= 0){
+                if(i == num_people){
+                    i =0;
+                    loops++;
+                }
+
+                int current_candy = (loops* num_people)+ (i+1);
+                if(candies - current_candy<0){
+                    res[i] += candies;
+                } else{
+                    res[i] += current_candy;
+                }
+                i++;
+                candies -=current_candy;
+            }
+            return res;
+    }
+}
