dfefa600bccd5f8ad196111de2e3bf447bf99ba3
Bishwa <bishwa.nayak@sap.com>
2020-09-03 15:52:38 +0530

    day

---
+++ src/august/week4/StreamChecker.java
@@ -0,0 +1,42 @@
+package august.week4;
+
+/**
+ * https://leetcode.com/explore/challenge/card/august-leetcoding-challenge/552/week-4-august-22nd-august-28th/3434/
+ */
+public class StreamChecker
+{
+//    class TrieNode{
+//        public TrieNode[] children;
+//        boolean isLeaf;
+//
+//        TrieNode(){
+//            children = new TrieNode[26];
+//            isLeaf=false;
+//        }
+//    }
+//
+//    public void insert(String word, TrieNode root) {
+//        TrieNode curr = root;
+//
+//        for(char c : word.toCharArray()){
+//            if(curr.children[c-'a']==null){
+//                curr.children[c-'a']= new TrieNode();
+//            }
+//            curr = curr.children[c-'a'];
+//        }
+//        curr.isLeaf = true;
+//    }
+//
+//    public StreamChecker(String[] words) {
+//        TrieNode root = new TrieNode();
+//        for(String word : words){
+//            insert(word, root);
+//        }
+//    }
+//
+//    public boolean query(char letter) {
+//
+//    }
+
+
+}
