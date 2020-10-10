92fa9789e2d5740815d920d2e84b790571bea890
Bishwa <bishwa.nayak@sap.com>
2020-10-10 16:34:37 +0530

    day

---
+++ src/oct/week2/MinimumNumberArrowsBurstBalloons.java
@@ -0,0 +1,21 @@
+package oct.week2;
+
+import java.util.Arrays;
+
+public class MinimumNumberArrowsBurstBalloons
+{
+    public int findMinArrowShots(int[][] points) {
+
+        if (points.length == 0) return 0;
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1],b[1]));
+        int arrowPos = points[0][1];
+        int result = 1;
+        for (int i = 1; i < points.length; i++) {
+            if (arrowPos >= points[i][0]) continue;
+            result++;
+            arrowPos = points[i][1];
+        }
+        return result;
+    }
+
+}
