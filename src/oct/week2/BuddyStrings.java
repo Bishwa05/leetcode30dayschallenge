d2ab84466edfbb6409e4143c4e51464650019052
Bishwa <bishwa.nayak@sap.com>
2020-10-12 15:34:24 +0530

    day

---
+++ src/oct/week2/BuddyStrings.java
@@ -0,0 +1,30 @@
+package oct.week2;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BuddyStrings
+{
+    public boolean buddyStrings(String A, String B) {
+
+        if(A.length() != B.length()) return false;
+        if(A.equals(B)){
+            Set<Character> s = new HashSet<>();
+            for(char c : A.toCharArray()){
+                s.add(c);
+            }
+            return s.size()<A.length();
+        }
+
+        List<Integer> diff = new ArrayList<>();
+
+        for(int i=0; i<A.length(); i++){
+            if(A.charAt(i) != B.charAt(i)){
+                diff.add(i);
+            }
+        }
+        return diff.size() ==2 && A.charAt(diff.get(0)) == B.charAt(diff.get(1)) && A.charAt(diff.get(1)) == B.charAt(diff.get(0));
+    }
+}
