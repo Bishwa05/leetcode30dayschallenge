5beec3fb117bbce80b20163288903c680fb28996
Bishwa <bishwa.nayak@sap.com>
2020-10-26 00:20:12 +0530

    day

--- src/oct/week3/StoneGameIV.java
+++ src/oct/week3/StoneGameIV.java
@@ -28,16 +28,30 @@ public class StoneGameIV
 {
     Boolean[] dp = new Boolean[100001];
 
-    public boolean winnerSqaureGame(int n){
+    public boolean winnerSqaureGameTopDown(int n){
         if(n == 0) return false;
         if(dp[n] != null) return dp[n];
         boolean winner = false;
         for(int i =1; i*i <= n; i++){
-            if(!winnerSqaureGame(n-i*i)){
+            if(!winnerSqaureGameTopDown(n-i*i)){
                 winner = true;
                 break;
             }
         }
         return dp[n] = winner;
+    }
+
+
+    public boolean winnerSqaureGameBottomUp(int n){
+        boolean dp[] = new boolean[n+1];
+        for(int i=1; i<=n; i++){
+            for(int j =1; j*j<=i; j++){
+                if(!dp[i- j*j]){
+                    dp[i]= true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
     }
 }
