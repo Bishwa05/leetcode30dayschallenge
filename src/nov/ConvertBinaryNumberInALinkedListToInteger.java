4017328aca592619596c7d64ed66d40eef16a79c
Bishwa <bishwa.nayak@sap.com>
2020-11-02 11:34:38 +0530

    day

---
+++ src/nov/ConvertBinaryNumberInALinkedListToInteger.java
@@ -0,0 +1,43 @@
+package nov;
+
+import may.ListNode;
+
+public class ConvertBinaryNumberInALinkedListToInteger
+{
+
+
+    public int getDecimalValue(ListNode head) {
+
+        int length = 0;
+        if(head == null) return 0;
+        int result = 0;
+
+        ListNode curr = head;
+
+        while(curr != null){
+            curr = curr.next;
+            length++;
+        }
+        length --;
+        ListNode curr2 = head;
+
+
+        while(curr2!= null){
+            if(curr2.val !=0)
+                result += Math.pow(2, length);
+            length--;
+            curr2= curr2.next;
+        }
+
+        return result;
+    }
+
+    public int getDecimalValue2(ListNode head) {
+        int num = head.val;
+        while (head.next != null) {
+            num = (num << 1) | head.next.val;
+            head = head.next;
+        }
+        return num;
+    }
+}
