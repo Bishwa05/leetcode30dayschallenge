bd68dfb99a3652f295fcef5218538c392962cde9
Bishwa <bishwa.nayak@sap.com>
2020-11-03 22:21:48 +0530

    day

---
+++ src/nov/ConsecutiveCharacters.java
@@ -0,0 +1,24 @@
+package nov;
+
+public class ConsecutiveCharacters
+{
+    public int maxPower(String s) {
+        if(s.length() ==1) return 1;
+        if(s.length() ==0) return 0;
+        int res =1;
+        int p =1;
+        char c = s.charAt(0);
+
+        for(int j =1; j<s.length(); j++){
+            if(c== s.charAt(j)){
+                p++;
+            }else{
+                res = Math.max(res,p);
+                p=1;
+            }
+            c= s.charAt(j);
+        }
+        res = Math.max(res,p);
+        return res;
+    }
+}
