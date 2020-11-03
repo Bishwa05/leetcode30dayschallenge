338ace64ecf9772ef5a44e0d562475fd4b420258
Bishwa <bishwa.nayak@sap.com>
2020-11-03 22:15:51 +0530

    day

---
+++ src/nov/InsertionSortList.java
@@ -0,0 +1,28 @@
+package nov;
+
+import may.ListNode;
+
+public class InsertionSortList
+{
+    public ListNode insertionSortList(ListNode head){
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = head;
+
+        while(curr !=null){
+            ListNode temp = curr.next;
+            ListNode prev = dummy;
+            ListNode nxt = dummy.next;
+
+            while(nxt != null){
+                if(nxt.val > curr.val)
+                    break;
+                prev = nxt;
+                nxt = nxt.next;
+            }
+            curr.next = nxt;
+            prev.next = curr;
+            curr = temp;
+        }
+        return dummy.next;
+    }
+}
