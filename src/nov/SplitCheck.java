04338310641061665edc3453f61ef9e36619f887
Bishwa <bishwa.nayak@sap.com>
2020-11-07 18:53:03 +0530

    day

---
+++ src/nov/SplitCheck.java
@@ -0,0 +1,10 @@
+package nov;
+
+public class SplitCheck
+{
+    public static void main(String arg[]){
+        String x = "Hello";
+        String arr[] = x.split(",");
+        System.out.println(arr[0]);
+    }
+}
