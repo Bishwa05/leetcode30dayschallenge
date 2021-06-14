3703619aa990d89f05e9e4100c0dc25c78bf1d39
Bishwa <bishwa.nayak@sap.com>
2021-06-15 01:25:10 +0530

    day

---
+++ src/jun21/week2/MaximumUnitsOnATruck.java
@@ -0,0 +1,33 @@
+package jun21.week2;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MaximumUnitsOnATruck
+{
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(b[1], a[1]));
+
+        for(int[] arr : boxTypes){
+            pq.offer(arr);
+        }
+
+        int res = 0;
+
+        while(truckSize>0 && pq.size()>0){
+            int[] element = pq.poll();
+
+            int k = element[0];
+            if(truckSize> k){
+                res+= k*element[1];
+            } else{
+                res+= element[1]*truckSize;
+            }
+            truckSize -= k;
+
+        }
+        return res;
+
+    }
+}
