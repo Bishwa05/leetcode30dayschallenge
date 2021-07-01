b90b8ce4005c4c9bbd1f7376870a4b0a27aa7875
Bishwa <bishwa.nayak@sap.com>
2021-07-01 23:20:34 +0530

    day

---
+++ src/july21/GrayCode.java
@@ -0,0 +1,39 @@
+package july21;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class GrayCode
+{
+    public List<Integer> grayCode(int n){
+        List<Integer> result = new ArrayList<>();
+        result.add(0);
+
+        Set<Integer> isPresent = new HashSet<>();
+        isPresent.add(0);
+        grayCodeHelper(result, n, isPresent);
+        return result;
+    }
+
+    public boolean grayCodeHelper(List<Integer> result, int n, Set<Integer> isPresent){
+        if(result.size() ==(1<<n)) return true;
+
+        int current = result.get(result.size()-1);
+
+        for(int i =0; i<n; i++){
+            int next = current ^ (i<<i);
+            if(isPresent.contains(next)){
+                isPresent.add(next);
+                result.add(next);
+                // if valid sequence found no need to search further
+                if(grayCodeHelper(result, n, isPresent)) return true;
+                // if no valid sequence found delete the last added number and continue search;
+                isPresent.remove(next);
+                result.remove(result.size()-1);
+            }
+        }
+        return false;
+    }
+}
