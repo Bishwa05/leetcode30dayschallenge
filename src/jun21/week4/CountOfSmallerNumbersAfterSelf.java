ebdf918d92b59c4bc5b431fa71370c640e3a82f3
Bishwa <bishwa.nayak@sap.com>
2021-07-06 00:06:00 +0530

    day

---
+++ src/jun21/week4/CountOfSmallerNumbersAfterSelf.java
@@ -0,0 +1,10 @@
+package jun21.week4;
+
+import java.util.*;
+
+public class CountOfSmallerNumbersAfterSelf
+{
+    public List<Integer> countSmaller(int[] nums) {
+        return new ArrayList<>();
+    }
+}
