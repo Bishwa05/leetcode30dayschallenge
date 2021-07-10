d02cc7cb768da1217ffd46a5f85c1a4b36aa9707
Bishwa <bishwa.nayak@sap.com>
2021-07-10 22:58:36 +0530

    day

--- src/july21/week2/MaximumLengthOfRepeatedSubarray.java
+++ src/july21/week2/MaximumLengthOfRepeatedSubarray.java
@@ -39,4 +39,19 @@ public class MaximumLengthOfRepeatedSubarray
         int res = m.findLength(A, B);
         System.out.println(res);
     }
+
+    public int findLengthDP(int[] A, int[] B){
+        int ans =0;
+        int[][] memo = new int[A.length+1][B.length+1];
+
+        for(int i = A.length; i>=0; i--){
+            for(int j = B.length; j>=0; j--){
+                if(A[i] == B[j]){
+                  memo[i][j] = memo[i+1][j+1] +1;
+                  if(ans <memo[i][j]) ans = memo[i][j];
+                }
+            }
+        }
+        return ans;
+    }
 }
