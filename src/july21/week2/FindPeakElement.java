f1710b34e483ec2bd8fed7ca2a0dfc7bb216c8e4
Bishwa <bishwa.nayak@sap.com>
2021-07-13 22:50:40 +0530

    day

---
+++ src/july21/week2/FindPeakElement.java
@@ -0,0 +1,21 @@
+package july21.week2;
+
+public class FindPeakElement
+{
+    public int findPeakElement(int[] nums) {
+
+        int l =0;
+        int r = nums.length-1;
+
+        while(l<r) {
+            int mid = (l+r) >>1;
+            if(nums[mid]> nums[mid+1]){
+                r = mid;
+            } else{
+                l = mid +1;
+            }
+        }
+        return l;
+    }
+
+}
